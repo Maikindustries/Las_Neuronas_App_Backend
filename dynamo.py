d4dc6e59dd6288bbae23b9b2f249d06b98184e63
Javier Moreno <javemmor@gmail.com>
2022-09-15 01:34:53 -0500

    m

--- dynamo.py
+++ dynamo.py
@@ -24,23 +24,31 @@ def get_db_items():
     #print(items)
     return items['Items']
 
-def serve_planet_stats():
+def serve_planet_stats(items):
     planets = [0, 0, 0]
-    items = get_db_items()
     for item in items:
         if item['transported'] == '1':
             planets[int(item['origin'])] += 1
     result = {'Earth': planets[0], "Mars": planets[1], "Europe": planets[2]}
     return result
 
-def serve_destination_stats():
+def serve_destination_stats(items):
     destinations = [0, 0, 0]
-    items = get_db_items()
     for item in items:
         if item['transported'] == '1':
             destinations[int(item['destination'])] += 1
     result = {'55 Cancri e': destinations[0], "TRAPPIST-1e": destinations[1], "PSO J318.5-22": destinations[2]}
     return result
-    
+
+def serve_avg_age(items):
+    count = 0
+    sum = 0
+    for item in items:
+        if item['transported'] == '1':
+            count += 1
+            sum += item['age']
+    return {'avg': sum / count, 'count': count}
+ 
 def serve_stats():
-    serve_planet_stats()
+    items = get_db_items()
+    return {"planets": serve_planet_stats(items), "destinations": serve_destination_stats(items), "age": serve_avg_age(items)}
--- tests.py
+++ tests.py
@@ -1,4 +1,3 @@
 import dynamo
 
-print(dynamo.serve_planet_stats())
-print(dynamo.serve_destination_stats())
+print(dynamo.serve_stats())
